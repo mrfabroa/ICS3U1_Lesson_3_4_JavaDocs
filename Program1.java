a4b4fbc181856ca16b5f1e6df2c31cf00f120fc8
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2024-04-11 13:00:43 -0400

    Update Program1.java

--- Program1.java
+++ Program1.java
@@ -1,13 +1,27 @@
+  /**
+  * Demonstrate the use of javadocs
+  * @author: E.Fabroa
+  */
+
+
 class Program1 extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * Top level method to execute the program
+  * @author: E.Fabroa
   */
-  
-  public void run() {
-    // your code goes here
-    
-    
+  public void run(){
+    System.out.println(diff(10,5));
+  }
+
+  /**
+  * Calculates the difference between two numbers
+  * @author E.Fabroa
+  * @param num1 the first number
+  * @param num2 the second number
+  * @return the difference between the two numbers
+  */
+  private int diff(int num1, int num2){
+    return num1 - num2;
   }
 }
