df5d6eadad888479b9434cab15f7bace6cec05af
mrfabroa <eric.fabroa@ycdsbk12.ca>
2024-04-12 14:28:14 -0400

    update

--- Program1.java
+++ Program1.java
@@ -15,13 +15,23 @@ class Program1 extends ConsoleProgram {
   }
 
   /**
-  * Calculates the difference between two numbers
-  * @author E.Fabroa
-  * @param num1 the first number
-  * @param num2 the second number
-  * @return the difference between the two numbers
-  */
+   * Compute the difference between two numbers
+   * @param num1 the first number
+   * @param num2 the second number
+   * @return the difference between num1 and num2
+   */
   private int diff(int num1, int num2){
     return num1 - num2;
+  }
+
+  /**
+   * Compute the product of two integers
+   * @param intNum1 the first number
+   * @param intNum2 the second number
+   * @return the product of the first and second number
+   * @author E.Fabroa
+   */
+  private int multiply(int intNum1, int intNum2){
+    return intNum1 * intNum2;
   }
 }
